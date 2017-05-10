34baa8d0e67673a55eff6f7434aba778dd99e099
glassjoseph <glassjoseph@gmail.com>
2017-05-10 08:31:37 -0600

    add reddit_service

--- app/services/reddit_service.rb
+++ app/services/reddit_service.rb
@@ -12,12 +12,27 @@ class RedditService
 
 
 # /subreddits/mine/subscriber
-  def subreddits
-    @conn.get("/subreddits/mine/subscriber/")
+#user.subreddits?
+  def self.subreddits
+    @conn.headers["Authorization"] = "bearer #{current_user.token}"
+    response = @conn.get("/subreddits/mine/subscriber/")
+    response = JSON.parse(response.body, symbolize_names: true)
+    response[:data][:children].each do |subreddit|
+      name = subreddit[:data][:display_name]
+      description = subreddit[:data][:description]
+    end
   end
+
+
+
+    #Make a model?
+
 
 
 
   # ["Authorization"] = "bearer #{current_user.token}"
 
+  # Do we create subreddit model? I think so. Then we can make user.subreddits has_many
+
 end
+#response[:data][:children].first[:data].  /:description :display_name
