30487990047237715bc2b18e7f1603844c5bf7c4
glassjoseph <glassjoseph@gmail.com>
2017-05-10 17:21:29 -0600

    test reddit_service_spec

--- spec/services/reddit_service_spec.rb
+++ spec/services/reddit_service_spec.rb
@@ -5,20 +5,17 @@ describe RedditService do
     it "returns all of a users subreddits" do
 
       #consider moving to user?
-      subreddits = RedditService.subreddits
-      subreddit = subreddits.first
+      VCR.use_cassette("users_subreddits") do
+        subreddits = RedditService.subreddits
+        subreddit = subreddits.first
 
-      expect(subreddits).to be_an(Array)
-      expect(subreddit).to be_a(Hash)
-      expect(subreddit).to have_key(:display_name)
-      expect(subreddit).to have_key(:description)
-      expect(subreddit[:display_name]).to be_a(String)
-      expect(subreddit[:description]).to be_a(String)
-
-
+        expect(subreddits).to be_an(Array)
+        expect(subreddit).to be_a(Hash)
+        expect(subreddit).to have_key(:display_name)
+        expect(subreddit).to have_key(:description)
+        expect(subreddit[:display_name]).to be_a(String)
+        expect(subreddit[:description]).to be_a(String)
+      end
     end
   end
-
-
-
 end
