b805f103658d1217b580f17a7e823bc925d360a1
glassjoseph <glassjoseph@gmail.com>
2017-05-10 17:19:49 -0600

    add subreddit model

---
+++ app/models/subreddit.rb
@@ -0,0 +1,15 @@
+class Subreddit < ApplicationRecord
+  belongs_to :user
+  validates :display_name, uniqueness: true
+  #
+  # def self.from_omniauth(auth_info)
+  #   subreddit = where(uid: auth_info[:uid]).first_or_create do |new_subreddit|
+  #     new_subreddit.name                = auth_info.display_name
+  #     new_subreddit.name               = auth_info.info.name
+  #     new_subreddit.token              = auth_info.credentials.token
+  #     new_subreddit.token_expiration   = auth_info.credentials.expires_at
+  #   end
+  #   subreddit
+  # end
+
+end
