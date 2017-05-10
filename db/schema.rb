9190019bef70f450e74b9c7a6b4c8b1e04a58f35
glassjoseph <glassjoseph@gmail.com>
2017-05-10 17:24:35 -0600

    show subreddits in home view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509005506) do
+ActiveRecord::Schema.define(version: 20170510195418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subreddits", force: :cascade do |t|
+    t.string  "display_name"
+    t.string  "description"
+    t.string  "url"
+    t.integer "subscribers"
+    t.string  "title"
+    t.string  "icon_img"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_subreddits_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
@@ -26,4 +37,5 @@ ActiveRecord::Schema.define(version: 20170509005506) do
     t.datetime "updated_at",       null: false
   end
 
+  add_foreign_key "subreddits", "users"
 end
